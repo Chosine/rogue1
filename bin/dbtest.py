592a622459ea3c986d7ce7985d0ee06b34f21e05
Chosine <98840205+Chosine@users.noreply.github.com>
2020-03-24 11:29:59 +0000

    0.7.7

--- bin/dbtest.py
+++ bin/dbtest.py
@@ -7,4 +7,5 @@ import os
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
 import config
 from models import Superblock, Proposal, GovernanceObject, Setting, Signal, Vote, Outcome
-from models import VoteSi
+from models import VoteSignals, VoteOutcomes
+fro
