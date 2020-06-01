a00dad8cd72d33d84b6bc9608187976dc61e239a
Chosine <98840205+Chosine@users.noreply.github.com>
2020-06-01 16:23:02 +0000

    initial empty check-in

--- bin/rehex.py
+++ bin/rehex.py
@@ -5,4 +5,6 @@ import pdb
 from pprint import pprint
 import sys
 import os
-sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
+import gobytelib
+# ======
