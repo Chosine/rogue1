0db9affbb739aa8340951e88d2ac890ee7c0bc56
Chosine <98840205+Chosine@users.noreply.github.com>
2020-06-12 19:57:09 +0000

    Cleanup else after return

--- bin/sentinel.py
+++ bin/sentinel.py
@@ -9,3 +9,30 @@ import misc
 from gobyted import GoByteDaemon
 from models import Superblock, Proposal, GovernanceObject
 from models import VoteSignals, VoteOutcomes, Transient
+import socket
+from misc import printdbg
+import time
+from bitcoinrpc.authproxy import JSONRPCException
+import signal
+import atexit
+import random
+from scheduler import Scheduler
+import argparse
+
+
+# sync gobyted gobject list with our local relational DB backend
+def perform_gobyted_object_sync(gobyted):
+    GovernanceObject.sync(gobyted)
+
+
+def prune_expired_proposals(gobyted):
+    # vote delete for old proposals
+    for proposal in Proposal.expired(gobyted.superblockcycle()):
+        proposal.vote(gobyted, VoteSignals.delete, VoteOutcomes.yes)
+
+
+def attempt_superblock_creation(gobyted):
+    import gobytelib
+
+    if not gobyted.is_masternode():
+        print("We are not a Masternode... can't submit superblocks!")
