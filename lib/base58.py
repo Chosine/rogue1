0f219acc8dc94690a9eeb43098762d13ed4e9b38
Chosine <98840205+Chosine@users.noreply.github.com>
2020-06-18 12:59:23 +0000

    ARM: 7696/1: Fix kexec by setting outer_cache.inv_all for Feroceon

--- lib/base58.py
+++ lib/base58.py
@@ -23,3 +23,30 @@ if str != bytes:
 
 __b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 __b58base = len(__b58chars)
+b58chars = __b58chars
+
+
+def b58encode(v):
+    """ encode v, which is a string of bytes, to base58.
+    """
+    long_value = 0
+    for (i, c) in enumerate(v[::-1]):
+        long_value += (256**i) * ord(c)
+
+    result = ''
+    while long_value >= __b58base:
+        div, mod = divmod(long_value, __b58base)
+        result = __b58chars[mod] + result
+        long_value = div
+    result = __b58chars[long_value] + result
+
+    # Bitcoin does a little leading-zero-compression:
+    # leading 0-bytes in the input become leading-1s
+    nPad = 0
+    for c in v:
+        if c == '\0':
+            nPad += 1
+        else:
+            break
+
+    return (__b58chars[0] * nPad) + result
