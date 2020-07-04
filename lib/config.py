8e9de5c5dd2a0df7e306086d237cd861a3b5d0e3
Chosine <98840205+Chosine@users.noreply.github.com>
2020-07-04 06:46:36 +0000

    add template

--- lib/config.py
+++ lib/config.py
@@ -9,4 +9,5 @@ default_sentinel_config = os.path.normpath(
     os.path.join(os.path.dirname(__file__), '../sentinel.conf')
 )
 sentinel_config_file = os.environ.get('SENTINEL_CONFIG', default_sentinel_config)
-sentinel_cfg = GoByteConfig.tokenize(sentinel_
+sentinel_cfg = GoByteConfig.tokenize(sentinel_config_file)
+sentinel_version
