e564fec0e7cecf2f5c382f58a97a74c151fa8376
Chosine <98840205+Chosine@users.noreply.github.com>
2020-07-06 13:05:03 +0000

    Staging: android: binder: Fix memory leak on thread/process exit

--- lib/config.py
+++ lib/config.py
@@ -15,4 +15,5 @@ sentinel_version = "1.5.0"
 
 def get_gobyte_conf():
     if sys.platform == 'win32':
-        gobyte_conf = os.path.join(os.getenv('APPDATA')
+        gobyte_conf = os.path.join(os.getenv('APPDATA'), "GoByteCore/gobyte.conf")
+   
