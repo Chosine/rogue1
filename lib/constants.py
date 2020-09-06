d441a0ec5858f8afd47d0f6de9bc49cb52df17f7
Chosine <98840205+Chosine@users.noreply.github.com>
2020-09-06 09:15:40 +0000

    API doc

--- lib/constants.py
+++ lib/constants.py
@@ -1,3 +1,4 @@
 # for constants which need to be accessed by various parts of Sentinel
 
-# skip proposals on superblock creation if the SB
+# skip proposals on superblock creation if the SB isn't within the fudge window
+SUPERBLOC
