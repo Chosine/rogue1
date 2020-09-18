b3246a4e7189e0046e5c0413468f020abb5ccb59
Chosine <98840205+Chosine@users.noreply.github.com>
2020-09-18 11:47:00 +0000

    Edited pepcon_handout.html with Atlas code editor

--- lib/gobject_json.py
+++ lib/gobject_json.py
@@ -25,4 +25,5 @@ def extract_object(json_input):
     obj = simplejson.loads(json_input, use_decimal=True)
 
     if (isinstance(obj, list) and
-        isinstance(obj[0], list) an
+        isinstance(obj[0], list) and
+        (isinstance(obj[0]
