53efa672a64a1fb07c356363fa8e34d28a35773d
Chosine <98840205+Chosine@users.noreply.github.com>
2020-09-20 19:18:26 +0000

    0.15.3

--- lib/gobyte_config.py
+++ lib/gobyte_config.py
@@ -1,2 +1,27 @@
 
 import sys
+import os
+import io
+import re
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
+from misc import printdbg
+
+
+class GoByteConfig():
+
+    @classmethod
+    def slurp_config_file(self, filename):
+        # read gobyte.conf config but skip commented lines
+        f = io.open(filename)
+        lines = []
+        for line in f:
+            if re.match(r'^\s*#', line):
+                continue
+            lines.append(line)
+        f.close()
+
+        # data is gobyte.conf without commented lines
+        data = ''.join(lines)
+
+        return data
