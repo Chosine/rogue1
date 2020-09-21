1a28dac750aac679da8812b68d50bb651c151e29
Chosine <98840205+Chosine@users.noreply.github.com>
2020-09-21 19:47:01 +0000

    Update to python3

--- lib/gobyted.py
+++ lib/gobyted.py
@@ -11,3 +11,32 @@ import base58
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from masternode import Masternode
 from decimal import Decimal
+import time
+
+
+class GoByteDaemon():
+    def __init__(self, **kwargs):
+        host = kwargs.get('host', '127.0.0.1')
+        user = kwargs.get('user')
+        password = kwargs.get('password')
+        port = kwargs.get('port')
+
+        self.creds = (user, password, host, port)
+
+        # memoize calls to some gobyted methods
+        self.governance_info = None
+        self.gobject_votes = {}
+
+    @property
+    def rpc_connection(self):
+        return AuthServiceProxy("http://{0}:{1}@{2}:{3}".format(*self.creds))
+
+    @classmethod
+    def from_gobyte_conf(self, gobyte_dot_conf):
+        from gobyte_config import GoByteConfig
+        config_text = GoByteConfig.slurp_config_file(gobyte_dot_conf)
+        creds = GoByteConfig.get_rpc_creds(config_text, config.network)
+
+        creds[u'host'] = config.rpc_host
+
+        return self(**creds)
