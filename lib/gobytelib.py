179fd13e1641669df5e811bd2ed66e0b7c7e2573
Chosine <98840205+Chosine@users.noreply.github.com>
2020-10-14 06:40:15 +0000

    Updated to Swift 3

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -2,4 +2,6 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
-impor
+import base58
+import hashlib
+i
