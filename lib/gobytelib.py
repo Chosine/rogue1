c8bbd0b5d2b80cfa56cbabfd89b2b49e74bf318d
Chosine <98840205+Chosine@users.noreply.github.com>
2020-10-16 05:28:56 +0000

    Rename test

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -4,4 +4,5 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 import base58
 import hashlib
-i
+import re
+from decimal import Decim
