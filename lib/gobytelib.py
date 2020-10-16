4b691d2fe62b9df10738bc65ca8d21ac4dece12b
Chosine <98840205+Chosine@users.noreply.github.com>
2020-10-16 15:22:44 +0000

    [pre-commit.ci] pre-commit autoupdate

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -5,4 +5,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 import base58
 import hashlib
 import re
-from decimal import Decim
+from decimal import Decimal
+import simplejson
+import
