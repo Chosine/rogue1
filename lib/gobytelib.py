7c6893dbc8a13c6357d78366981704301b75ced6
Chosine <98840205+Chosine@users.noreply.github.com>
2020-12-22 08:09:41 +0000

    Replace deprecated code.

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -73,4 +73,6 @@ def elect_mn(**kwargs):
 
 
 def parse_masternode_status_vin(status_vin_string):
-    status_vin_string_regex = re.compile(r'CTxIn\(COutPoint\(([0-9a-zA-Z
+    status_vin_string_regex = re.compile(r'CTxIn\(COutPoint\(([0-9a-zA-Z]+),\s*(\d+)\),')
+
+    m = 
