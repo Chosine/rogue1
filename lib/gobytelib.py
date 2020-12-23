65de2f0557be12baf476e949c356d9124bb424fa
Chosine <98840205+Chosine@users.noreply.github.com>
2020-12-23 14:46:26 +0000

    New translations messages.json (Korean)

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -75,4 +75,6 @@ def elect_mn(**kwargs):
 def parse_masternode_status_vin(status_vin_string):
     status_vin_string_regex = re.compile(r'CTxIn\(COutPoint\(([0-9a-zA-Z]+),\s*(\d+)\),')
 
-    m = status_vin_string_regex.match(s
+    m = status_vin_string_regex.match(status_vin_string)
+
+    # To Suppo
