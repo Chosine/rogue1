c1828fb6702414fffa01b742cf10e10414b632a4
Chosine <98840205+Chosine@users.noreply.github.com>
2021-01-03 19:28:22 +0000

    Add sample image

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -94,4 +94,5 @@ def parse_masternode_status_vin(status_vin_string):
 
 def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
     from models import Superblock, GovernanceObject, Proposal
-    from constants import SUPER
+    from constants import SUPERBLOCK_FUDGE_WINDOW
+    impor
