b376b0bd48c1f1a75686f757ec258eae692146c3
Chosine <98840205+Chosine@users.noreply.github.com>
2021-01-13 08:58:06 +0000

    OAK-6210 - Provide a consistent and extensible way to handle oak-run options while creating NodeStore

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -103,4 +103,4 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
         return None
 
     budget_allocated = Decimal(0)
-    fudge = SUPERBLOCK_FUDGE_WINDOW  # fudge-factor
+    fudge = SUPERBLOCK_FUDGE_WINDOW  # fudge-factor to allow for slightly incorrect est
