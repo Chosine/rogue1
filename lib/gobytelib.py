3144117b76f9df3a619ca1ea4427e6d4c1f7e306
Chosine <98840205+Chosine@users.noreply.github.com>
2021-01-20 12:01:29 +0000

    Change formatting

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -116,4 +116,5 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
                 fmt_string % (
                     proposal.name,
                     proposal.rank,
-           
+                    proposal.object_hash,
+  
