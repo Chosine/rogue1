9d69d7664b6ecc3abd42e4f797e478bd4a4d1660
Chosine <98840205+Chosine@users.noreply.github.com>
2021-01-29 18:36:38 +0000

    Chmod

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -124,4 +124,5 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
             continue
 
         # skip proposals if the SB isn't within the Proposal time window...
-        window_start =
+        window_start = proposal.start_epoch - fudge
+   
