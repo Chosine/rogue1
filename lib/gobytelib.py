04f71d335780b52aa329792184b5761ec1f64b50
Chosine <98840205+Chosine@users.noreply.github.com>
2021-01-31 06:11:33 +0000

    0.13.5

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -125,4 +125,4 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
 
         # skip proposals if the SB isn't within the Proposal time window...
         window_start = proposal.start_epoch - fudge
-   
+        window_end = proposal.end_
