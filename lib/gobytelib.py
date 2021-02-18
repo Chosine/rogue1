7870a528f1be151286c55ad8b5cf6bdcb5c21bb4
Chosine <98840205+Chosine@users.noreply.github.com>
2021-02-18 12:27:24 +0000

    code readability

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -132,4 +132,5 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
         printdbg("\tsb_epoch_time: %s" % epoch2str(sb_epoch_time))
 
         if (sb_epoch_time < window_start or sb_epoch_time > window_end):
-           
+            printdbg(
+                fm
