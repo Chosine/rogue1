a199d4b7831c2a191269e6a5f9b334d3687f05c7
Chosine <98840205+Chosine@users.noreply.github.com>
2021-02-18 08:50:20 +0000

    Styling fixes

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -131,4 +131,5 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
         printdbg("\twindow_end: %s" % epoch2str(window_end))
         printdbg("\tsb_epoch_time: %s" % epoch2str(sb_epoch_time))
 
-        if (sb_epoch_time < window_start or sb_epoch_time
+        if (sb_epoch_time < window_start or sb_epoch_time > window_end):
+           
