ade2250afe46fa633c1b35caff7ebfcac0bbe443
Chosine <98840205+Chosine@users.noreply.github.com>
2021-02-20 10:40:04 +0000

    add login form

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -137,4 +137,5 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
                     proposal.name,
                     proposal.rank,
                     proposal.object_hash,
-       
+                    proposal.payment_amount,
+ 
