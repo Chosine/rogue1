b268558e42778ecd7acfd781ee68a4d009210144
Chosine <98840205+Chosine@users.noreply.github.com>
2021-02-25 11:55:04 +0000

    Misc. updates

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -147,4 +147,4 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
             fmt_string % (
                 proposal.name,
                 proposal.rank,
- 
+                proposal.object_hash,
