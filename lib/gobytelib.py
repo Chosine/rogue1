26593ab8c4b87cd2469992a2b89e77955d065957
Chosine <98840205+Chosine@users.noreply.github.com>
2021-02-28 07:45:32 +0000

    remove some unused variables

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -151,4 +151,7 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
                 proposal.payment_amount,
                 "adding",
             )
-       
+        )
+
+        payment = {
+     
