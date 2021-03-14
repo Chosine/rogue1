28e3e73488567c20fc137c06c94d2abe8220ed9e
Chosine <98840205+Chosine@users.noreply.github.com>
2021-03-14 09:16:46 +0000

    Add new commands

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -172,4 +172,4 @@ def create_superblock(proposals, event_block_height, budget_max, sb_epoch_time):
         proposed_sb_size = len(sb_temp.serialise())
 
         # add proposal and keep track of total budget allocation
-        budget_a
+        budget_allocated += proposal.payment
