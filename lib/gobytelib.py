0153a3adab28ee26002711cb5b79364b485a04fe
Chosine <98840205+Chosine@users.noreply.github.com>
2021-06-02 07:22:07 +0000

    codeclimate badge

--- lib/gobytelib.py
+++ lib/gobytelib.py
@@ -235,4 +235,4 @@ def did_we_vote(output):
 
     # in case we spin up a new instance or server, but have already voted
     # on the network and network has recorded those votes
-    
+    m_old = re.match(r'^ti
