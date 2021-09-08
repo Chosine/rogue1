f05460963a0be192c70d068972a52c1cf536c779
Chosine <98840205+Chosine@users.noreply.github.com>
2021-09-08 08:29:29 +0000

    Update search.php

--- lib/init.py
+++ lib/init.py
@@ -3,3 +3,27 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
+
+
+def is_valid_python_version():
+    version_valid = False
+
+    ver = sys.version_info
+    if (2 == ver.major) and (7 <= ver.minor):
+        version_valid = True
+
+    if (3 == ver.major) and (4 <= ver.minor):
+        version_valid = True
+
+    return version_valid
+
+
+def python_short_ver_str():
+    ver = sys.version_info
+    return "%s.%s" % (ver.major, ver.minor)
+
+
+def are_deps_installed():
+    installed = False
+
+    try:
