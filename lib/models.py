abf10b5cf59f575495f0abff5bb9de834bc69790
Chosine <98840205+Chosine@users.noreply.github.com>
2021-12-17 21:13:34 +0000

    Change background image

--- lib/models.py
+++ lib/models.py
@@ -19,3 +19,29 @@ from bitcoinrpc.authproxy import JSONRPCException
 try:
     import urllib.parse as urlparse
 except ImportError:
+    import urlparse
+
+# our mixin
+from governance_class import GovernanceClass
+
+db = config.db
+db.connect()
+
+
+# TODO: lookup table?
+GOBYTED_GOVOBJ_TYPES = {
+    'proposal': 1,
+    'superblock': 2,
+}
+GOVOBJ_TYPE_STRINGS = {
+    1: 'proposal',
+    2: 'trigger',  # it should be trigger here, not superblock
+}
+
+# schema version follows format 'YYYYMMDD-NUM'.
+#
+# YYYYMMDD is the 4-digit year, 2-digit month and 2-digit day the schema
+# changes were added.
+#
+# NUM is a numerical version of changes for that specific date. If the date
+# changes, the NUM resets to 1.
