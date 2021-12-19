d9e4bd6b7e7927547eacf7b512301df5697797d3
Chosine <98840205+Chosine@users.noreply.github.com>
2021-12-19 17:35:47 +0000

    Removed outdated comment.

--- lib/models.py
+++ lib/models.py
@@ -45,3 +45,32 @@ GOVOBJ_TYPE_STRINGS = {
 #
 # NUM is a numerical version of changes for that specific date. If the date
 # changes, the NUM resets to 1.
+SCHEMA_VERSION = '20170111-1'
+
+# === models ===
+
+
+class BaseModel(playhouse.signals.Model):
+    class Meta:
+        database = db
+
+    @classmethod
+    def is_database_connected(self):
+        return not db.is_closed()
+
+
+class GovernanceObject(BaseModel):
+    parent_id = IntegerField(default=0)
+    object_creation_time = IntegerField(default=int(time.time()))
+    object_hash = CharField(max_length=64)
+    object_parent_hash = CharField(default='0')
+    object_type = IntegerField(default=0)
+    object_revision = IntegerField(default=1)
+    object_fee_tx = CharField(default='')
+    yes_count = IntegerField(default=0)
+    no_count = IntegerField(default=0)
+    abstain_count = IntegerField(default=0)
+    absolute_yes_count = IntegerField(default=0)
+
+    class Meta:
+        db_table = 'governance_objects'
