9f42995541442bcd15d25f9079e0e773e880460c
Chosine <98840205+Chosine@users.noreply.github.com>
2022-01-08 10:53:10 +0000

    Update pull request template

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -1,3 +1,4 @@
 import sys
 import os
-sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__fi
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
+import init
