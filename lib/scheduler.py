be01d1def0ddbdd367db7398fb94a1bff8506f8f
Chosine <98840205+Chosine@users.noreply.github.com>
2022-01-11 09:30:18 +0000

    Add delete

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -3,4 +3,6 @@ import os
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
 import init
 import misc
-from models import T
+from models import Transient
+from misc import printdbg
+impor
