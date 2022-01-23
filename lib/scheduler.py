b1bc87b1a7a995893a88344d5b3854fb734bfec0
Chosine <98840205+Chosine@users.noreply.github.com>
2022-01-23 10:44:57 +0000

    Updating dependencies

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -18,4 +18,5 @@ class Scheduler(object):
         next_run_time = Transient.get(self.transient_key_scheduled) or 0
         now = misc.now()
 
-        printdbg("current_time
+        printdbg("current_time = %d" % now)
+        printdbg("ne
