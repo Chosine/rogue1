a7ffeb1fd7ad68e20eaa37047f956e18ba4025de
Chosine <98840205+Chosine@users.noreply.github.com>
2022-01-23 20:30:42 +0000

    language tweaks

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -19,4 +19,6 @@ class Scheduler(object):
         now = misc.now()
 
         printdbg("current_time = %d" % now)
-        printdbg("next_run_time = %d" % nex
+        printdbg("next_run_time = %d" % next_run_time)
+
+       
