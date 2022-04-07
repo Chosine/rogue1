4a718fd3c6d795e8490e62aec0b256073664b2ea
Chosine <98840205+Chosine@users.noreply.github.com>
2022-04-07 11:53:46 +0000

    hwmon: (pmbus/ltc2978) Fix temperature reporting

--- share/run.sh
+++ share/run.sh
@@ -54,4 +54,5 @@ if [ ! -f /sentinel/sentinel.conf ]; then
   if [ -n "$DB_PASSWORD" ]; then
     echo "db_password=${DB_PASSWORD}" >> /sentinel/sentinel.conf
   fi
-  if [ -n "$DB_CHARSET" ]; t
+  if [ -n "$DB_CHARSET" ]; then
+    echo "db_charset=${
