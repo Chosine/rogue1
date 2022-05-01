099663b4528b9f86c0e04316d024b8385f43fbce
Chosine <98840205+Chosine@users.noreply.github.com>
2022-05-01 19:49:14 +0000

    Removed useless files.

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -7,3 +7,5 @@ os.environ['SENTINEL_ENV'] = 'test'
 os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../test_sentinel.conf'))
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../../lib')))
 import misc
+import config
+from models 
