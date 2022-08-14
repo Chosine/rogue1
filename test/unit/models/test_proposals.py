32056d4c0c35a97db600b881942e706fc6ec9312
Chosine <98840205+Chosine@users.noreply.github.com>
2022-08-14 16:48:23 +0000

    Update main.cc

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -111,4 +111,6 @@ def test_proposal_is_valid(proposal):
     # ============================================================
 
     proposal.name = '   heya!@209h '
-    assert proposal.is_va
+    assert proposal.is_valid() is False
+
+    proposal.n
