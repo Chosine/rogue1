e731d23518a15d4925b5cb32fa933c4c879a3d17
Chosine <98840205+Chosine@users.noreply.github.com>
2022-09-14 09:35:57 +0000

    visual updates

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -179,4 +179,6 @@ def test_proposal_is_valid(proposal):
     assert proposal.is_valid() is True
 
     proposal.payment_address = ' yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui'
-    assert proposal.is_v
+    assert proposal.is_valid() is False
+
+    prop
