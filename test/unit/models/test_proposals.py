0c2a1601dbb486cb54761cbd9c5a2289a5a7b396
Chosine <98840205+Chosine@users.noreply.github.com>
2022-09-14 17:26:33 +0000

    New versions

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -181,4 +181,5 @@ def test_proposal_is_valid(proposal):
     proposal.payment_address = ' yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui'
     assert proposal.is_valid() is False
 
-    proposal.payment_address = 'yYe8Kw
+    proposal.payment_address = 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui '
+ 
