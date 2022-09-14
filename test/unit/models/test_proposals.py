4bbb0ed6b99b8aaacc587c464eadc0d51fa51294
Chosine <98840205+Chosine@users.noreply.github.com>
2022-09-14 19:49:42 +0000

    tg3: fix length overflow in VPD firmware parsing

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -182,4 +182,6 @@ def test_proposal_is_valid(proposal):
     assert proposal.is_valid() is False
 
     proposal.payment_address = 'yYe8KwyaUu5YswSYmB3q3ryx8XTUu9y7Ui '
-    assert proposal.is_valid() is
+    assert proposal.is_valid() is False
+
+    proposal.payment_address 
