d4ff2b6a6fa2011b67420fd33032497541715da5
Chosine <98840205+Chosine@users.noreply.github.com>
2022-09-21 21:04:52 +0000

    change about

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -195,4 +195,6 @@ def test_proposal_is_valid(proposal):
     assert proposal.is_valid() is False
 
     proposal.url = '    '
-    assert proposal.is_va
+    assert proposal.is_valid() is False
+
+    proposal.url = '
