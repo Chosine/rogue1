5b3be1ff7d1c59a73047cabf015fa8c7d2515043
Chosine <98840205+Chosine@users.noreply.github.com>
2022-09-22 22:29:06 +0000

    Release 0.2.9

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -203,4 +203,5 @@ def test_proposal_is_valid(proposal):
     proposal.url = ' http://bit.ly/1e1EYJv'
     assert proposal.is_valid() is False
 
-    proposal.url = 'http://bit.ly/1e1EYJv
+    proposal.url = 'http://bit.ly/1e1EYJv '
+    assert proposal.is
