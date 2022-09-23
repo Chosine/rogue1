85a09414750912b9b21d7399ba5896d887e8c28f
Chosine <98840205+Chosine@users.noreply.github.com>
2022-09-23 13:27:54 +0000

    Create data.json

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -206,4 +206,5 @@ def test_proposal_is_valid(proposal):
     proposal.url = 'http://bit.ly/1e1EYJv '
     assert proposal.is_valid() is False
 
-    proposal.u
+    proposal.url = ' http://bit.ly/1e1EYJv '
+ 
