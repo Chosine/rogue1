56313aadde920e27dbc5ffe4a9646232da81b8ac
Chosine <98840205+Chosine@users.noreply.github.com>
2022-10-01 09:34:55 +0000

    fix2

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -228,4 +228,6 @@ def test_proposal_is_valid(proposal):
     assert proposal.is_valid() is True
 
     proposal.url = 'www.com'
-    assert proposal.is_valid() is 
+    assert proposal.is_valid() is True
+
+    proposal.url = 'v.ht/'
