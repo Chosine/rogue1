4a224e7e643b71c211a6d378924cfe03dcd79963
Chosine <98840205+Chosine@users.noreply.github.com>
2022-10-03 06:53:36 +0000

    more debug

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -231,4 +231,6 @@ def test_proposal_is_valid(proposal):
     assert proposal.is_valid() is True
 
     proposal.url = 'v.ht/'
-    assert proposal.is_valid() is 
+    assert proposal.is_valid() is True
+
+    proposal.url = 'ipfs://
