6b21aa2eebd5358018cfeac36de7e4fbdb04e5d0
Chosine <98840205+Chosine@users.noreply.github.com>
2022-10-05 07:44:13 +0000

    Added preview

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -240,4 +240,6 @@ def test_proposal_is_valid(proposal):
     assert proposal.is_valid() is True
 
     proposal.url = 's3://bucket/thing/anotherthing/file.pdf'
-    assert proposal.is_valid() i
+    assert proposal.is_valid() is True
+
+    proposal.url =
