5a94f2da84949a42c45e9fe638d85214750057b4
Chosine <98840205+Chosine@users.noreply.github.com>
2022-11-28 14:17:48 +0000

    setfacl removes part of ACL when setting POSIX ACLs to Samba

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -286,4 +286,4 @@ def test_approved_and_ranked(go_list_proposals):
     for item in go_list_proposals:
         (go, subobj) = GovernanceObject.import_gobject_from_gobyted(gobyted, item)
 
-    prop_list = Proposal.approved_and_ranked(proposal_q
+    prop_list = Proposal.approved_and_ranked(proposal_quorum=1, next_superblock_max_b
