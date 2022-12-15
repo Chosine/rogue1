028b9fc27f4b33751b2eea22973095b898ea7707
Chosine <98840205+Chosine@users.noreply.github.com>
2022-12-15 15:14:03 +0000

    Fix uninitialized variables

---
+++ test/unit/models/test_superblocks.py
@@ -0,0 +1,24 @@
+
+import pytest
+import sys
+import os
+import time
+os.environ['SENTINEL_ENV'] = 'test'
+os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../test_sentinel.conf'))
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../../lib')))
+import misc
+import config
+from models import GovernanceObject, Proposal, Superblock, Vote
+
+
+# clear DB tables before each execution
+def setup():
+    # clear tables first...
+    Vote.delete().execute()
+    Proposal.delete().execute()
+    Superblock.delete().execute()
+    GovernanceObject.delete().execute()
+
+
+def teardown():
+    pass
