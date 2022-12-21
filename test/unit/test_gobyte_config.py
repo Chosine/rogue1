39fc8dcd3d7235d9f25fce693e874604f6533bcd
Chosine <98840205+Chosine@users.noreply.github.com>
2022-12-21 12:40:37 +0000

    Add more cave details

--- test/unit/test_gobyte_config.py
+++ test/unit/test_gobyte_config.py
@@ -3,4 +3,5 @@ import os
 import sys
 import re
 os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../test_sentinel.conf'))
-os.envir
+os.environ['SENTINEL_ENV'] = 'test'
+s
