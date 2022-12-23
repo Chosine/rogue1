9d3f37c65c89a8b15e4fa58d84ddcb4601c065f3
Chosine <98840205+Chosine@users.noreply.github.com>
2022-12-23 11:13:17 +0000

    W

--- test/unit/test_gobyte_config.py
+++ test/unit/test_gobyte_config.py
@@ -4,4 +4,4 @@ import sys
 import re
 os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../test_sentinel.conf'))
 os.environ['SENTINEL_ENV'] = 'test'
-sys.path.append(os.path.normpath(os.path.join(os.path.dirname(
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../lib')))
