5f178fb46efe6959d74e0770f886c70bf980f8e1
Chosine <98840205+Chosine@users.noreply.github.com>
2023-01-05 14:54:25 +0000

    Better exception handling.

--- test/unit/test_gobyte_config.py
+++ test/unit/test_gobyte_config.py
@@ -52,4 +52,5 @@ def test_get_rpc_creds():
     assert creds.get('password') == 's00pers33kr1t'
     assert creds.get('port') == 8000
 
-    no_port_specified = re.sub('\nrpcport=.*?\n', '\n', gobyte_
+    no_port_specified = re.sub('\nrpcport=.*?\n', '\n', gobyte_conf(), re.M)
+    creds = 
