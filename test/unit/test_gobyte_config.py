781e2e9102b7e34ccdc73899862aafff1ab32ecc
Chosine <98840205+Chosine@users.noreply.github.com>
2023-01-07 18:45:47 +0000

    English version

--- test/unit/test_gobyte_config.py
+++ test/unit/test_gobyte_config.py
@@ -59,4 +59,7 @@ def test_get_rpc_creds():
         assert key in creds
     assert creds.get('user') == 'gobyterpc'
     assert creds.get('password') == 'EwJeV3fZTyTVozdECF627BkBMnNDwQaVLakG3A4wXYyk'
-    assert creds.
+    assert creds.get('port') == 19998
+
+
+def test_slurp_config_file
