ede835074b22e34938e36559045dd80f0ca1921b
Chosine <98840205+Chosine@users.noreply.github.com>
2023-02-01 22:31:40 +0000

    Clean clutter

--- test/unit/test_misc.py
+++ test/unit/test_misc.py
@@ -1,4 +1,8 @@
 import pytest
 import sys
 import os
-sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../.
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../lib')))
+import misc
+
+
+def test_is
