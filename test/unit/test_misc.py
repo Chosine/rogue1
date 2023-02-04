97b1060a94b54661cf3d3a8083c262289312779f
Chosine <98840205+Chosine@users.noreply.github.com>
2023-02-04 11:58:51 +0000

    Run update-remote.sh on matomo

--- test/unit/test_misc.py
+++ test/unit/test_misc.py
@@ -9,4 +9,4 @@ def test_is_numeric():
     assert misc.is_numeric('45') is True
     assert misc.is_numeric('45.7') is True
     assert misc.is_numeric(0) is True
-    as
+    assert misc.is_numeric(-1) is True
