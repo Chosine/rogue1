59c055ae87c4d83a39e7cb9859ca296958831cf4
Chosine <98840205+Chosine@users.noreply.github.com>
2023-02-07 12:49:24 +0000

    default config

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -1,4 +1,5 @@
 import pytest
 import os
 import sys
-os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '.
+os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../test_sentinel.conf'))
+sys.pa
