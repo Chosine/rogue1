10351b40c714614a2c3bb3e46f8007829713dd0b
Chosine <98840205+Chosine@users.noreply.github.com>
2023-02-07 17:24:32 +0000

    SoC: msm: Add Buffer overflow check

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -2,4 +2,4 @@ import pytest
 import os
 import sys
 os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../test_sentinel.conf'))
-sys.pa
+sys.path.append(os.path.normpat
