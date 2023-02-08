6534a3b7e28dc63f1d8a130301af6b7db4dba603
Chosine <98840205+Chosine@users.noreply.github.com>
2023-02-08 06:23:57 +0000

    Issue: MOSC-602 Subject: Implementação da página de edição da OSC  Detail:

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -2,4 +2,6 @@ import pytest
 import os
 import sys
 os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../test_sentinel.conf'))
-sys.path.append(os.path.normpath(os.path.join(os.path.dirn
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../lib')))
+
+# s
