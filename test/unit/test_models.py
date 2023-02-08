a85128ca6bcbcc98d34f22ae403b239bc3583753
Chosine <98840205+Chosine@users.noreply.github.com>
2023-02-08 09:27:31 +0000

    Update error handling

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -4,4 +4,6 @@ import sys
 os.environ['SENTINEL_CONFIG'] = os.path.normpath(os.path.join(os.path.dirname(__file__), '../test_sentinel.conf'))
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../../lib')))
 
-# s
+# setup/teardown?
+
+
